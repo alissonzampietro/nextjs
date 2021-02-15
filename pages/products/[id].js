e12263cef669a8016cdabbe2c4c21b506635fb63
Alisson Zampietro <alisson.zampietro@bluepanda.pt>
2021-02-15 15:15:48 +0000

    feat: implement query params

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,5 +1,10 @@
+import {useRouter} from 'next/router'; 
+
 function Products() {
-    
+    const router = new useRouter();
+    const id = router.query.id
+
+    return <div>O id do produto é {id}</div>
 }
 
 module.exports = Products;
